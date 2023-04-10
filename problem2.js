8eb92d67e0520b1a054fd1b8213860d341b7a4b2
S NAVEEN KUMAR <navee1110@gmail.com>
2023-04-10 17:35:10 +0530

    switch

---
+++ problem2.js
@@ -0,0 +1,30 @@
+//syntax switc(variable){
+case assigned value : console.log("")
+}
+
+
+var number = 3;
+switch(number){
+  case 1 : console.log("English");
+  case 2 : console.log("Hindi");
+  case 3 : console.log("Kannada");
+  case 4 : console.log("Telugu");
+    default : console.log("Wrong input");
+}
+
+console.log("\n");
+
+var number = 6;
+switch(number){
+  case 1 : console.log("English");
+    break;
+  case 2 : console.log("Hindi");
+    break;
+  case 3 : console.log("Kannada");
+    break;
+  case 4 : console.log("Telugu");
+    break;
+    default : console.log("Wrong input");
+}
+
+console.log(true||false)
